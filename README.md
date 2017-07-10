aaef42d91769e0baa22eb7fa42976e47438573ab
Sebastian Correa <correa.m.sebastian@gmail.com>
2017-07-09 21:01:04 -0700

    added CustomHandlingStrategy

--- README.md
+++ README.md
@@ -26,3 +26,8 @@ Before running the tests make sure you are serving the app via `ng serve`.
 ## Further help
 
 To get more help on the Angular CLI use `ng help` or go check out the [Angular CLI README](https://github.com/angular/angular-cli/blob/master/README.md).
+
+
+## hybrid
+
+you can se the Angular route using http://localhost:4200/feature1/sub1#!/view1
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { RouterModule, UrlHandlingStrategy } from '@angular/router';
 import {UpgradeModule } from '@angular/upgrade/static'
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { Feature1Sub1Component } from './feature1-sub1/feature1-sub1.component';
 import { Feature1Sub2Component } from './feature1-sub2/feature1-sub2.component';
+
+// url strategy
+class CustomHandlingStrategy implements UrlHandlingStrategy {
+  shouldProcessUrl(url) { return url.toString().startsWith('/feature1') || url.toString() === '/'; }
+  extract(url) { return url; }
+  merge(url, whole) { return url; }
+}
+//
 
 @NgModule({
   declarations: [
@@ -24,7 +32,9 @@ import { Feature1Sub2Component } from './feature1-sub2/feature1-sub2.component';
       { path: 'feature1/sub2', component: Feature1Sub2Component }
     ])
   ],
-  providers: [],
+  providers: [
+    { provide: UrlHandlingStrategy, useClass: CustomHandlingStrategy }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
