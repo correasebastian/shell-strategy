74e658d264ece71d110385246b017e4fe236cd4a
Sebastian Correa <correa.m.sebastian@gmail.com>
2017-07-09 20:37:14 -0700

    shell in place and working

--- ng1/view1/view1.js
+++ ng1/view1/view1.js
@@ -4,7 +4,8 @@ angular.module('myApp.view1', ['ngRoute'])
 
 .config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/view1', {
-    templateUrl: 'view1/view1.html',
+    // templateUrl: 'view1/view1.html',
+    template: '<p>This is the partial for view 1.</p>',
     controller: 'View1Ctrl'
   });
 }])
--- ng1/view2/view2.js
+++ ng1/view2/view2.js
@@ -4,7 +4,12 @@ angular.module('myApp.view2', ['ngRoute'])
 
 .config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/view2', {
-    templateUrl: 'view2/view2.html',
+    // templateUrl: 'view2/view2.html',
+    template: `<p>This is the partial for view 2.</p>
+              <p>
+                Showing of 'interpolate' filter:
+                {{ 'Current version is v%VERSION%.' | interpolate }}
+              </p>`,
     controller: 'View2Ctrl'
   });
 }])
