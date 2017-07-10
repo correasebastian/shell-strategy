7762fa1f2fc0246e90285d89cc9d620e58e45b33
Sebastian Correa <correa.m.sebastian@gmail.com>
2017-07-09 21:10:44 -0700

    from hash to html5 location true

--- ng1/app.js
+++ ng1/app.js
@@ -8,7 +8,8 @@ angular.module('myApp', [
   'myApp.version'
 ]).
 config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
-  $locationProvider.hashPrefix('!');
+  // $locationProvider.hashPrefix('!');
+   $locationProvider.html5Mode(true);
   // $locationProvider.config
 
   $routeProvider
